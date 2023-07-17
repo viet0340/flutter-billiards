15be6168e6646a758e0c5a308f675e7a7275c7ee
vietvv <viet0340@gmail.com>
2023-07-17 15:53:19 +0700

    UP

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -17,6 +17,7 @@ class _SettingScreenState extends State<SettingScreen> {
   final TextEditingController _controllerTimeBreak = TextEditingController();
   final TextEditingController _controllerTimeExtension =
       TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
@@ -35,131 +36,136 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     final timerModel = Provider.of<TimerModel>(context, listen: false);
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          color: Colors.white,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: TextFormField(
-                  controller: _controllerTimeInitialized,
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    labelText: 'Time Loop', // Đặt văn bản cho nhãn
-                    labelStyle:
-                        TextStyle(color: Colors.white), // Đặt màu chữ cho nhãn
-                    hintStyle: TextStyle(
-                        color: Colors.grey), // Đặt màu chữ cho văn bản gợi ý
-                    prefixIcon: Icon(Icons.person,
-                        color: Colors
-                            .white), // Đặt biểu tượng tiền tố và màu của nó
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .blue), // Đặt màu viền khi trường được kích hoạt
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .blue), // Đặt màu viền khi trường được tập trung
-                    ),
-                  ),
-                  onChanged: (value) {
-                    timerModel.changeTime(
-                        time: value.isEmpty ? 0 : int.parse(value),
-                        type: 'initialized');
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter time loop';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: TextFormField(
-                  controller: _controllerTimeBreak,
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    labelText: 'Time Break',
-                    labelStyle: TextStyle(color: Colors.white),
-                    hintStyle: TextStyle(color: Colors.grey),
-                    prefixIcon: Icon(Icons.person, color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    timerModel.changeTime(
-                        time: value.isEmpty ? 0 : int.parse(value),
-                        type: 'break');
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter time break';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: TextFormField(
-                  controller: _controllerTimeExtension,
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    labelText: 'Time Extension',
-                    labelStyle: TextStyle(color: Colors.white),
-                    hintStyle: TextStyle(color: Colors.grey),
-                    prefixIcon: Icon(Icons.person, color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    timerModel.changeTime(
-                        time: value.isEmpty ? 0 : int.parse(value),
-                        type: 'extension');
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter time extension';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-            ],
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            elevation: 0,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
-        ),
-      ),
-    );
+          body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TextFormField(
+                      controller: _controllerTimeInitialized,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        labelText: 'Time Loop', // Đặt văn bản cho nhãn
+                        labelStyle: TextStyle(
+                            color: Colors.white), // Đặt màu chữ cho nhãn
+                        hintStyle: TextStyle(
+                            color:
+                                Colors.grey), // Đặt màu chữ cho văn bản gợi ý
+                        prefixIcon: Icon(Icons.person,
+                            color: Colors
+                                .white), // Đặt biểu tượng tiền tố và màu của nó
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .blue), // Đặt màu viền khi trường được kích hoạt
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .blue), // Đặt màu viền khi trường được tập trung
+                        ),
+                      ),
+                      onChanged: (value) {
+                        timerModel.changeTime(
+                            time: value.isEmpty ? 0 : int.parse(value),
+                            type: 'initialized');
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter time loop';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TextFormField(
+                      controller: _controllerTimeBreak,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        labelText: 'Time Break',
+                        labelStyle: TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        prefixIcon: Icon(Icons.person, color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        timerModel.changeTime(
+                            time: value.isEmpty ? 0 : int.parse(value),
+                            type: 'break');
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter time break';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TextFormField(
+                      controller: _controllerTimeExtension,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        labelText: 'Time Extension',
+                        labelStyle: TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        prefixIcon: Icon(Icons.person, color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        timerModel.changeTime(
+                            time: value.isEmpty ? 0 : int.parse(value),
+                            type: 'extension');
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter time extension';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
